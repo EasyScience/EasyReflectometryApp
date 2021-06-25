bc3515b84b4e4fc602c5cbd2219f75252ffbeb69
Andrew Sazonov <Andrew.Sazonov@icloud.com>
2021-06-25 13:41:04 +0200

    Add periodictable to PyInstaller data

--- tools/Scripts/FreezeApp.py
+++ tools/Scripts/FreezeApp.py
@@ -5,7 +5,7 @@ import os, sys
 import importlib
 import glob
 import PySide2, shiboken2
-import refnx, refl1d
+import refnx, refl1d, periodictable
 import easyCore, easyReflectometryLib, easyAppGui, easyAppLogic
 import Functions, Config
 from PyInstaller.__main__ import run as pyInstallerMain
@@ -32,6 +32,7 @@ def addedData():
             {'from': importlib.import_module(lib).__path__[0], 'to': lib},
             {'from': refnx.__path__[0], 'to': 'refnx'},
             {'from': refl1d.__path__[0], 'to': 'refl1d'},
+            {'from': periodictable.__path__[0], 'to': 'periodictable'},
             {'from': easyCore.__path__[0], 'to': 'easyCore'},
             {'from': easyReflectometryLib.__path__[0], 'to': 'easyReflectometryLib'},
             {'from': easyAppLogic.__path__[0], 'to': 'easyAppLogic'},
