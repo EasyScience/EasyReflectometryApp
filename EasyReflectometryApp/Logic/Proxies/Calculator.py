7e73c8a58f13de1987fe97c221f03cc379ecc450
Andrew McCluskey <andrew.mccluskey@ess.eu>
2022-05-30 16:16:25 +0200

    Limit to using only refnx for now

--- EasyReflectometryApp/Logic/Proxies/Calculator.py
+++ EasyReflectometryApp/Logic/Proxies/Calculator.py
@@ -22,7 +22,7 @@ class CalculatorProxy(QObject):
 
     @Property('QVariant', notify=dummySignal)
     def calculatorNames(self):
-        return self.parent._interface[0].available_interfaces
+        return self.parent._interface[0].available_interfaces[:1]
 
     @Property(int, notify=calculatorChanged)
     def currentCalculatorIndex(self):
@@ -34,7 +34,7 @@ class CalculatorProxy(QObject):
         if self.currentCalculatorIndex == new_index:
             return
         new_name = self.calculatorNames[new_index]
-        
+
         self.parent._model_proxy._model.switch_interface(new_name)
         self.parent._fitter_proxy.eFitter.initialize(self.parent._model_proxy._model,
                                                       self.parent._interface[0].fit_func)
