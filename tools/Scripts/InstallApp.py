7ae6888c64304c7d642fe5f2aa9476139d256bb5
arm61 <andrew@mccluskey.scot>
2022-11-04 13:55:19 +0100

    RunApp updated

--- tools/Scripts/InstallApp.py
+++ tools/Scripts/InstallApp.py
@@ -7,22 +7,28 @@
 __author__ = ["github.com/AndrewSazonov", "github.com/arm61"]
 __version__ = '0.0.1'
 
-import os, sys
-import Functions, Config
-
+import os
+import sys
+import Functions
+import Config
 
 CONFIG = Config.Config()
+
 
 def setupExePath() -> str:
     """
     :return: Path to product executable.
     """
     d = {
-        'macos': os.path.join(CONFIG.setup_full_name, 'Contents', 'MacOS', CONFIG.setup_name),
-        'ubuntu': CONFIG.setup_full_name,
-        'windows': CONFIG.setup_full_name
+        'macos':
+        os.path.join(CONFIG.setup_full_name, 'Contents', 'MacOS', CONFIG.setup_name),
+        'ubuntu':
+        CONFIG.setup_full_name,
+        'windows':
+        CONFIG.setup_full_name
     }
     return os.path.join(CONFIG.dist_dir, d[CONFIG.os])
+
 
 def runInstallerSilently():
     """
@@ -32,16 +38,14 @@ def runInstallerSilently():
         message = f'install {CONFIG.app_name}'
         silent_script = CONFIG['ci']['scripts']['silent_install']
         silent_script_path = os.path.join(CONFIG.scripts_dir, silent_script)
-        args = {
-            'installer':     setupExePath(),
-            'silent_script': silent_script_path
-        }
+        args = {'installer': setupExePath(), 'silent_script': silent_script_path}
         Functions.installSilently(**args)
     except Exception as exception:
         Functions.printFailMessage(message, exception)
         sys.exit()
     else:
         Functions.printSuccessMessage(message)
+
 
 if __name__ == "__main__":
     runInstallerSilently()
--- tools/Scripts/RunApp.py
+++ tools/Scripts/RunApp.py
@@ -1,35 +1,53 @@
-__author__ = "github.com/AndrewSazonov"
+# SPDX-FileCopyrightText: 2022 EasyReflectometry contributors
+# <support@easyreflectometry.org>
+# SPDX-License-Identifier: BSD-3-Clause
+# © 2021-2022 Contributors to the EasyReflectometry project
+# <https://github.com/easyScience/EasyReflectometryApp>
+
+__author__ = ["github.com/AndrewSazonov", "github.com/arm61"]
 __version__ = '0.0.1'
 
-import os, sys
-import Functions, Config
-
+import os
+import sys
+import Functions
+import Config
 
 CONFIG = Config.Config()
 
-def appExePath():
+
+def appExePath() -> str:
+    """
+    :return: Application executable path.
+    """
     d = {
-        'macos': os.path.join(CONFIG.installation_dir, CONFIG.app_full_name, 'Contents', 'MacOS', CONFIG.app_name),
-        'ubuntu': os.path.join(CONFIG.installation_dir, CONFIG.app_name, CONFIG.app_full_name),
-        'windows': os.path.join(CONFIG.installation_dir, CONFIG.app_name, CONFIG.app_full_name)
+        'macos':
+        os.path.join(CONFIG.installation_dir, CONFIG.app_full_name, 'Contents', 'MacOS',
+                     CONFIG.app_name),
+        'ubuntu':
+        os.path.join(CONFIG.installation_dir, CONFIG.app_name, CONFIG.app_full_name),
+        'windows':
+        os.path.join(CONFIG.installation_dir, CONFIG.app_name, CONFIG.app_full_name)
     }
     return d[CONFIG.os]
 
+
 def runApp():
+    """
+    Launch and run application.
+    """
     Functions.printNeutralMessage(f'Installed application exe path: {appExePath()}')
     try:
         message = f'run {CONFIG.app_name}'
         if len(sys.argv) == 1:
             Functions.run(appExePath())
         else:
-            #if 'test' in sys.argv[1:]:
-            #    Functions.createDir(CONFIG.screenshots_dir)
             Functions.run(appExePath(), *sys.argv[1:])
     except Exception as exception:
         Functions.printFailMessage(message, exception)
         sys.exit()
     else:
         Functions.printSuccessMessage(message)
+
 
 if __name__ == "__main__":
     runApp()
