37a875afc97cbff354fcdfd40ae35d47a8a82c7d
arm61 <andrew@mccluskey.scot>
2023-01-18 12:04:06 +0100

    Fix ci in pyproject toml

--- utils.py
+++ utils.py
@@ -54,22 +54,22 @@ def extraDict():
     release_tag = f'v{app_version}'
     release_title = f'Version {app_version} ({build_date})'
 
-    return {'ci': { 'paths': { 'python_packages_path': python_packages_path },
-                    'git': { 'build_date': build_date,
-                             'date_for_qtifw': date_for_qtifw,
-                             'release_tag': release_tag,
-                             'release_title': release_title,
-                             'branch_name': branch_name,
-                             'branch_url': branch_url,
-                             'commit_sha_short': commit_sha_short,
-                             'commit_url': commit_url } } }
+    return { 'paths': { 'python_packages_path': python_packages_path },
+             'git': { 'build_date': build_date,
+                      'date_for_qtifw': date_for_qtifw,
+                      'release_tag': release_tag,
+                      'release_title': release_title,
+                      'branch_name': branch_name,
+                      'branch_url': branch_url,
+                      'commit_sha_short': commit_sha_short,
+                      'commit_url': commit_url } } 
 
 def extraToml():
     return toml.dumps(extraDict())
 
 def updatePyProjectToml():
     output_dict = proj()
-    output_dict.update(extraDict())
+    output_dict['ci'].update(extraDict())
     with open(pyproject_toml(), 'w') as f:
         toml.dump(output_dict, f)
 
