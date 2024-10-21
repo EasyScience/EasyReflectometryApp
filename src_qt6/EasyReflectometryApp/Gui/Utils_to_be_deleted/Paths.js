4b0aa681f20322fae09bf576d5c86545cc5cac06
Andreas Pedersen <andreas.pedersen@ess.eu>
2024-10-21 14:02:34 +0200

    remove utils

--- src_qt6/EasyReflectometryApp/Gui/Utils_to_be_deleted/Paths.js
+++
@@ -1,7 +0,0 @@
-function component(fileName)
-{
-    const dirPath = Qt.resolvedUrl("../Components")
-    //const dirPath = "../../Components"
-    const filePath = dirPath + "/" + fileName
-    return filePath
-}
