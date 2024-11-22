072452a1d387d148e1ae74ce6ffc4ece878ee3dc
Andreas Pedersen <andreas.pedersen@ess.eu>
2024-11-22 10:51:22 +0100

    adjustments

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# Version 1.0.0 (27 Nov 2024)
+
+Changed to Qt6
--- tools/Scripts/MakeInstaller.py
+++ tools/Scripts/MakeInstaller.py
@@ -2,15 +2,19 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # © 2021-2023 Contributors to the easyDiffraction project <https://github.com/easyScience/easyDiffractionApp>
 
-import os, sys
+import os
+import sys
 import time
-import requests
 import xml.dom.minidom
-import licensename, dephell_licenses
-import Functions, Config
 
+import Config
+import dephell_licenses
+import Functions
+import licensename
+import requests
 
 CONFIG = Config.Config(sys.argv[1], sys.argv[2])
+
 
 def qtifwSetupFileName():
     file_version = CONFIG['ci']['qtifw']['setup']['version']
@@ -19,8 +23,10 @@ def qtifwSetupFileName():
     file_ext = CONFIG['ci']['qtifw']['setup']['file_ext'][CONFIG.os]
     return f'{file_name_base}-{file_platform}-{file_version}.{file_ext}'
 
+
 def qtifwSetupDownloadDest():
     return os.path.join(CONFIG.download_dir, f'{qtifwSetupFileName()}')
+
 
 def urlOk(url):
     try:
@@ -36,6 +42,7 @@ def urlOk(url):
     except Exception as exception:
         Functions.printFailMessage(message, exception)
         return False
+
 
 def qtifwSetupDownloadUrl():
     repos = CONFIG['ci']['qtifw']['setup']['https_mirrors']
@@ -56,14 +63,16 @@ def qtifwSetupDownloadUrl():
         Functions.printSuccessMessage(message)
         return url
 
+
 def qtifwSetupExe():
     setup_name, _ = os.path.splitext(qtifwSetupFileName())
     d = {
         'macos': f'/Volumes/{setup_name}/{setup_name}.app/Contents/MacOS/{setup_name}',
         'ubuntu': qtifwSetupDownloadDest(),
-        'windows': qtifwSetupDownloadDest()
+        'windows': qtifwSetupDownloadDest(),
     }
     return d[CONFIG.os]
+
 
 def qtifwDirPath():
     home_dir = os.path.expanduser('~')
@@ -71,79 +80,88 @@ def qtifwDirPath():
     d = {
         'macos': f'{home_dir}/Qt/QtIFW-{qtifw_version}',
         'ubuntu': f'{home_dir}/Qt/QtIFW-{qtifw_version}',
-        'windows': f'C:\\Qt\\QtIFW-{qtifw_version}'
+        'windows': f'C:\\Qt\\QtIFW-{qtifw_version}',
     }
     return d[CONFIG.os]
+
 
 def setupBuildDirPath():
     setup_build_dir_suffix = CONFIG['ci']['app']['setup']['build_dir_suffix']
     return os.path.join(CONFIG.build_dir, f'{CONFIG.app_name}{setup_build_dir_suffix}')
 
+
 def configDirPath():
     return os.path.join(setupBuildDirPath(), CONFIG['ci']['app']['setup']['build']['config_dir'])
+
 
 def configXmlPath():
     return os.path.join(configDirPath(), CONFIG['ci']['app']['setup']['build']['config_xml'])
 
+
 def packagesDirPath():
     return os.path.join(setupBuildDirPath(), CONFIG['ci']['app']['setup']['build']['packages_dir'])
+
 
 def localRepositoryDir():
     repository_dir_suffix = CONFIG['ci']['app']['setup']['repository_dir_suffix']
     return os.path.join(f'{CONFIG.app_name}{repository_dir_suffix}', CONFIG.setup_os)
 
+
 def onlineRepositoryUrl():
     host = CONFIG['ci']['app']['setup']['ftp']['host']
     prefix = CONFIG['ci']['app']['setup']['ftp']['prefix']
     repo_subdir = CONFIG['ci']['app']['setup']['ftp']['repo_subdir']
-    #return f'https://{prefix}.{host}/{repo_subdir}/{CONFIG.setup_os}'
+    # return f'https://{prefix}.{host}/{repo_subdir}/{CONFIG.setup_os}'
     return f'ftp://u652432322.repo:easyDiffraction123@download.easydiffraction.org/{CONFIG.setup_os}'
+
 
 def installerConfigXml():
     try:
         message = f"create {CONFIG['ci']['app']['setup']['build']['config_xml']} content"
         app_url = CONFIG['release']['homepage']
         maintenance_tool_suffix = CONFIG['ci']['app']['setup']['maintenance_tool_suffix']
-        maintenance_tool_name = maintenance_tool_suffix #f'{CONFIG.app_name}{maintenance_tool_suffix}'
+        maintenance_tool_name = maintenance_tool_suffix  # f'{CONFIG.app_name}{maintenance_tool_suffix}'
         config_control_script = CONFIG['ci']['scripts']['config_control']
         config_style = CONFIG['ci']['scripts']['config_style']
         # https://doc.qt.io/qtinstallerframework/ifw-globalconfig.html
         # /Applications/easyTemplate/MaintenanceTool.app/Contents/MacOS/MaintenanceTool --addRepository http://easyscience.apptimity.com/easyTemplateRepo/official/macOS --updater
         # http://download.qt.io/online/qtsdkrepository/windows_x86/root/qt/
         # https://stackoverflow.com/questions/46455360/workaround-for-qt-installer-framework-not-overwriting-existing-installation/46614107#46614107
-        raw_xml = Functions.dict2xml({
-            'Installer': {
-                'Name': CONFIG.app_name,
-                'Version': CONFIG.app_version,
-                'Title': CONFIG.app_name,
-                'Publisher': CONFIG.app_name,
-                'ProductUrl': app_url,
-                #'Logo': 'logo.png',
-                'WizardStyle': 'Classic', #'Aero',
-                'WizardDefaultWidth': 900,
-                'WizardDefaultHeight': 600,
-                'StyleSheet': config_style,
-                'StartMenuDir' : CONFIG.family_name,
-                'TargetDir': CONFIG.installation_dir_for_qtifw,
-                #'CreateLocalRepository': 'true',
-                #'SaveDefaultRepositories': 'false',
-                #'RepositorySettingsPageVisible': 'false',
-                'RemoteRepositories': {
-                    'Repository': [
-                        {
-                            'Url': onlineRepositoryUrl(),
-                            'DisplayName': f'{CONFIG.app_name} {CONFIG.setup_os}_{CONFIG.setup_arch} repository',
-                            'Enabled': 1,
-                        }
-                    ]
-                },
-                'MaintenanceToolName': maintenance_tool_name,
-                'AllowNonAsciiCharacters': 'true',
-                'AllowSpaceInPath': 'true',
-                'InstallActionColumnVisible': 'false',
-                'ControlScript': config_control_script,
+        raw_xml = Functions.dict2xml(
+            {
+                'Installer': {
+                    'Name': CONFIG.app_name,
+                    'Version': CONFIG.app_version,
+                    'Title': CONFIG.app_name,
+                    'Publisher': CONFIG.app_name,
+                    'ProductUrl': app_url,
+                    #'Logo': 'logo.png',
+                    'WizardStyle': 'Classic',  #'Aero',
+                    'WizardDefaultWidth': 900,
+                    'WizardDefaultHeight': 600,
+                    'StyleSheet': config_style,
+                    'StartMenuDir': CONFIG.family_name,
+                    'TargetDir': CONFIG.installation_dir_for_qtifw,
+                    #'CreateLocalRepository': 'true',
+                    #'SaveDefaultRepositories': 'false',
+                    #'RepositorySettingsPageVisible': 'false',
+                    'RemoteRepositories': {
+                        'Repository': [
+                            {
+                                'Url': onlineRepositoryUrl(),
+                                'DisplayName': f'{CONFIG.app_name} {CONFIG.setup_os}_{CONFIG.setup_arch} repository',
+                                'Enabled': 1,
+                            }
+                        ]
+                    },
+                    'MaintenanceToolName': maintenance_tool_name,
+                    'AllowNonAsciiCharacters': 'true',
+                    'AllowSpaceInPath': 'true',
+                    'InstallActionColumnVisible': 'false',
+                    'ControlScript': config_control_script,
+                }
             }
-        })
+        )
         pretty_xml = xml.dom.minidom.parseString(raw_xml).toprettyxml()
     except Exception as exception:
         Functions.printFailMessage(message, exception)
@@ -152,34 +170,32 @@ def installerConfigXml():
         Functions.printSuccessMessage(message)
         return pretty_xml
 
+
 def appPackageXml():
     try:
-        message = f"create app package content"
+        message = f'create app package content'
         license_file = CONFIG['project']['license']['file']
         license_id = licensename.from_file(license_file)
         license_name = dephell_licenses.licenses.get_by_id(license_id).name
         license_name = license_name.replace('"', "'")
         requires_root = 'false'
-        raw_xml = Functions.dict2xml({
-            'Package': {
-                'DisplayName': CONFIG.app_name,
-                'Description': CONFIG['project']['description'],
-                'Version': CONFIG.app_version,
-                'ReleaseDate': CONFIG['ci']['app']['info']['date_for_qtifw'],
-                'Default': 'true',
-                #'SortingPriority': 100,
-                'Essential': 'true',
-                'ForcedInstallation': 'true',
-                'RequiresAdminRights': 'false',
-                'Licenses': {
-                    'License': {
-                        '@name': license_name,
-                        '@file': CONFIG.license_file
-                    }
-                },
-                'Script': CONFIG['ci']['scripts']['package_install'],
+        raw_xml = Functions.dict2xml(
+            {
+                'Package': {
+                    'DisplayName': CONFIG.app_name,
+                    'Description': CONFIG['project']['description'],
+                    'Version': CONFIG.app_version,
+                    'ReleaseDate': CONFIG['ci']['app']['info']['date_for_qtifw'],
+                    'Default': 'true',
+                    #'SortingPriority': 100,
+                    'Essential': 'true',
+                    'ForcedInstallation': 'true',
+                    'RequiresAdminRights': 'false',
+                    'Licenses': {'License': {'@name': license_name, '@file': CONFIG.license_file}},
+                    'Script': CONFIG['ci']['scripts']['package_install'],
+                }
             }
-        })
+        )
         pretty_xml = xml.dom.minidom.parseString(raw_xml).toprettyxml()
     except Exception as exception:
         Functions.printFailMessage(message, exception)
@@ -188,7 +204,8 @@ def appPackageXml():
         Functions.printSuccessMessage(message)
         return pretty_xml
 
-#def docsPackageXml():
+
+# def docsPackageXml():
 #    try:
 #        message = f"create docs package content"
 #        name = CONFIG['ci']['app']['setup']['build']['docs_package_name']
@@ -213,12 +230,11 @@ def appPackageXml():
 #        Functions.printSuccessMessage(message)
 #        return pretty_xml
 
+
 def downloadQtInstallerFramework():
     Functions.createDir(CONFIG.download_dir)
-    Functions.downloadFile(
-        url=qtifwSetupDownloadUrl(),
-        destination=qtifwSetupDownloadDest()
-    )
+    Functions.downloadFile(url=qtifwSetupDownloadUrl(), destination=qtifwSetupDownloadDest())
+
 
 def osDependentPreparation():
     message = f'prepare for os {CONFIG.os}'
@@ -231,6 +247,7 @@ def osDependentPreparation():
     else:
         Functions.printNeutralMessage(f'No preparation needed for os {CONFIG.os}')
 
+
 def installQtInstallerFramework():
     osDependentPreparation()
     if os.path.exists(qtifwDirPath()):
@@ -238,19 +255,13 @@ def installQtInstallerFramework():
         return
     try:
         message = f'install QtInstallerFramework to {qtifwDirPath()}'
-        Functions.run(
-            qtifwSetupExe(),
-            'install',
-            '--verbose',
-            '--confirm-command',
-            '--default-answer',
-            '--accept-licenses'
-        )
+        Functions.run(qtifwSetupExe(), 'install', '--verbose', '--confirm-command', '--default-answer', '--accept-licenses')
     except Exception as exception:
         Functions.printFailMessage(message, exception)
         sys.exit(1)
     else:
         Functions.printSuccessMessage(message)
+
 
 def createInstallerSourceDir():
     try:
@@ -265,12 +276,14 @@ def createInstallerSourceDir():
         Functions.copyFile(source=config_control_script_path, destination=configDirPath())
         Functions.copyFile(source=config_style_path, destination=configDirPath())
         # package: app
-        app_subdir_path =  os.path.join(packagesDirPath(), CONFIG['ci']['app']['setup']['build']['app_package_subdir'])
-        app_data_subsubdir_path =  os.path.join(app_subdir_path, CONFIG['ci']['app']['setup']['build']['data_subsubdir'])
-        app_meta_subsubdir_path =  os.path.join(app_subdir_path, CONFIG['ci']['app']['setup']['build']['meta_subsubdir'])
+        app_subdir_path = os.path.join(packagesDirPath(), CONFIG['ci']['app']['setup']['build']['app_package_subdir'])
+        app_data_subsubdir_path = os.path.join(app_subdir_path, CONFIG['ci']['app']['setup']['build']['data_subsubdir'])
+        app_meta_subsubdir_path = os.path.join(app_subdir_path, CONFIG['ci']['app']['setup']['build']['meta_subsubdir'])
         app_package_xml_path = os.path.join(app_meta_subsubdir_path, CONFIG['ci']['app']['setup']['build']['package_xml'])
         package_install_script_src = os.path.join(CONFIG.scripts_dir, CONFIG['ci']['scripts']['package_install'])
-        freezed_app_src = os.path.join(CONFIG.dist_dir, f"{CONFIG.app_name}{CONFIG['ci']['pyinstaller']['dir_suffix'][CONFIG.os]}")
+        freezed_app_src = os.path.join(
+            CONFIG.dist_dir, f"{CONFIG.app_name}{CONFIG['ci']['pyinstaller']['dir_suffix'][CONFIG.os]}"
+        )
         Functions.createDir(packagesDirPath())
         Functions.createDir(app_subdir_path)
         Functions.createDir(app_data_subsubdir_path)
@@ -287,19 +300,19 @@ def createInstallerSourceDir():
         ##docs_data_subsubdir_path = os.path.join(docs_subdir_path, CONFIG['ci']['app']['setup']['build']['data_subsubdir'])
         ##docs_meta_subsubdir_path = os.path.join(docs_subdir_path, CONFIG['ci']['app']['setup']['build']['meta_subsubdir'])
         ##docs_package_xml_path = os.path.join(docs_meta_subsubdir_path, CONFIG['ci']['app']['setup']['build']['package_xml'])
-        #docs_dir_src = CONFIG['ci']['project']['subdirs']['docs']['src']
-        #docs_dir_dest = CONFIG['ci']['project']['subdirs']['docs']['dest']
+        # docs_dir_src = CONFIG['ci']['project']['subdirs']['docs']['src']
+        # docs_dir_dest = CONFIG['ci']['project']['subdirs']['docs']['dest']
         ##Functions.createDir(docs_subdir_path)
         ##Functions.createDir(docs_data_subsubdir_path)
         ##Functions.createDir(docs_meta_subsubdir_path)
         ##Functions.createFile(path=docs_package_xml_path, content=docsPackageXml())
         ##Functions.copyDir(source=docs_dir_src, destination=os.path.join(docs_data_subsubdir_path, 'Documentation'))
-        #Functions.copyDir(source=docs_dir_src, destination=os.path.join(app_data_subsubdir_path, docs_dir_dest))
+        # Functions.copyDir(source=docs_dir_src, destination=os.path.join(app_data_subsubdir_path, docs_dir_dest))
         # package: examples
-        examples_dir_src = CONFIG['ci']['project']['subdirs']['examples']['src'].replace('{PROJECT_NAME}/', '')
-        examples_dir_dest = CONFIG['ci']['project']['subdirs']['examples']['dest']
-        Functions.copyDir(source=os.path.join(CONFIG['project']['name'], examples_dir_src),
-                          destination=os.path.join(app_data_subsubdir_path, examples_dir_dest))
+        # examples_dir_src = CONFIG['ci']['project']['subdirs']['examples']['src'].replace('{PROJECT_NAME}/', '')
+        # examples_dir_dest = CONFIG['ci']['project']['subdirs']['examples']['dest']
+        # Functions.copyDir(source=os.path.join(CONFIG['project']['name'], examples_dir_src),
+        #                  destination=os.path.join(app_data_subsubdir_path, examples_dir_dest))
         # TODO: change the handling of failure in all methods in Functions.py so they bubble up exceptions
         # TODO: remove this platform conditional once the above is done
     except Exception as exception:
@@ -307,6 +320,7 @@ def createInstallerSourceDir():
         sys.exit(1)
     else:
         Functions.printSuccessMessage(message)
+
 
 def createOfflineInstaller():
     try:
@@ -320,10 +334,13 @@ def createOfflineInstaller():
             qtifw_binarycreator_path,
             '--verbose',
             '--offline-only',
-            '-c', configXmlPath(),
-            '-p', packagesDirPath(),
-            '-t', qtifw_installerbase_path,
-            setup_exe_path
+            '-c',
+            configXmlPath(),
+            '-p',
+            packagesDirPath(),
+            '-t',
+            qtifw_installerbase_path,
+            setup_exe_path,
         )
     except Exception as exception:
         Functions.printFailMessage(message, exception)
@@ -331,24 +348,20 @@ def createOfflineInstaller():
     else:
         Functions.printSuccessMessage(message)
 
+
 def createOnlineRepositoryLocally():
     try:
         message = 'create online repository'
         qtifw_bin_dir_path = os.path.join(qtifwDirPath(), 'bin')
         qtifw_repogen_path = os.path.join(qtifw_bin_dir_path, 'repogen')
         repository_dir_path = os.path.join(CONFIG.dist_dir, localRepositoryDir())
-        Functions.run(
-            qtifw_repogen_path,
-            '--verbose',
-            '--update-new-components',
-            '-p', packagesDirPath(),
-            repository_dir_path
-        )
+        Functions.run(qtifw_repogen_path, '--verbose', '--update-new-components', '-p', packagesDirPath(), repository_dir_path)
     except Exception as exception:
         Functions.printFailMessage(message, exception)
         sys.exit(1)
     else:
         Functions.printSuccessMessage(message)
+
 
 def addFilesToLocalRepository():
     try:
@@ -362,10 +375,10 @@ def addFilesToLocalRepository():
         Functions.printSuccessMessage(message)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     downloadQtInstallerFramework()
     installQtInstallerFramework()
     createInstallerSourceDir()
     createOfflineInstaller()
-    #createOnlineRepositoryLocally()
-    #addFilesToLocalRepository()
+    # createOnlineRepositoryLocally()
+    # addFilesToLocalRepository()
