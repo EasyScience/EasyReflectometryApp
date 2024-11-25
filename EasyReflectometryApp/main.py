fcadc41acb1ffa7dd92dab0566de16eb2bbae248
Andreas Pedersen <andreas.pedersen@ess.eu>
2024-11-25 14:27:57 +0100

    path as string

--- EasyReflectometryApp/main.py
+++ EasyReflectometryApp/main.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
 
     if INSTALLER:
         path_main_qml = QUrl.fromLocalFile(CURRENT_DIR / 'EasyReflectometryApp' / 'main.qml')  # Running from installer
-        engine.addImportPath(QUrl.fromLocalFile(CURRENT_DIR))
+        engine.addImportPath(QUrl.fromLocalFile(CURRENT_DIR).toString())
         console.debug('Paths added where QML searches for components')
     else:
         path_main_qml = path_main_qml = CURRENT_DIR / 'main.qml'  # Running locally
